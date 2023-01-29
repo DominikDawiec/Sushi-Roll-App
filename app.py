c70e83e9441ed24483469cdf64151cec46416a06
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2023-01-29 19:37:26 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+# List of ingredients
+ingredients = ['smoked salmon', 'avocado', 'cucumber', 'crab', 'cream cheese', 'eel sauce', 'wasabi', 'soy sauce']
+
+# Sushi rolls and their ingredients
+sushi_rolls = {
+    'Seattle Roll': ['smoked salmon', 'avocado', 'cucumber'],
+    'Philadelphia Roll': ['cream cheese', 'smoked salmon', 'avocado'],
+    'Spicy Tuna Roll': ['tuna', 'spicy mayo', 'avocado'],
+    'Dragon Roll': ['eel', 'avocado', 'cucumber'],
+    'California Roll': ['crab', 'avocado', 'cucumber'],
+}
+
+# Function to get the list of rolls that can be made
+def get_rolls(ingredients):
+    available_rolls = []
+    for roll, ingr in sushi_rolls.items():
+        if set(ingr).issubset(ingredients):
+            available_rolls.append(roll)
+    return available_rolls
+
+# Streamlit app
+st.title("Sushi Roll Maker")
+
+selected_ingredients = st.multiselect("Select Ingredients", ingredients)
+
+if len(selected_ingredients) > 0:
+    st.write("You can make the following rolls:")
+    rolls = get_rolls(selected_ingredients)
+    st.write(*rolls)
+else:
+    st.write("Please select ingredients.")
