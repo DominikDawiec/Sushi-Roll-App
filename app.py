6cb8f340b7e72becf63dbc0609e0767a4e0927a2
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2023-01-29 20:27:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,12 @@ def get_rolls(ingredients):
 st.title("Sushi Roll Maker")
 
 selected_ingredients = []
+
+ingredient_images = {}
+for ingredient in ingredients:
+    ingredient_images[ingredient] = st.image(f"{ingredient}.jpg")
+
+# Checkbox for each ingredient
 for ingredient in ingredients:
     if st.checkbox(ingredient):
         selected_ingredients.append(ingredient)
@@ -32,7 +38,10 @@ if st.button("Load"):
     if len(selected_ingredients) > 0:
         st.write("You can make the following rolls:")
         rolls = get_rolls(selected_ingredients)
-        for roll in rolls:
-            st.write(f"- {roll} ({', '.join(sushi_rolls[roll])})")
+        if rolls:
+            for roll in rolls:
+                st.write(f"- {roll}")
+        else:
+            st.write("You cannot make any sushi rolls.")
     else:
         st.write("Please select ingredients.")
