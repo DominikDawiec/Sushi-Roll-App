432581ea66c66eefc9005b374ce94ef4993d749e
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2023-01-31 13:38:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ with col3:
     st.markdown("</center>", unsafe_allow_html=True)
 
 st.markdown("<center>", unsafe_allow_html=True)
-if st.button("Make sushi 🔪", width=100, key="make_sushi_button"):
+if st.button("Make sushi 🔪", width=100):
     st.markdown("<center>", unsafe_allow_html=True)
     if len(selected_ingredients) > 0:
         bar = st.progress(0)
